a726948576992d85183fd6c4e09561cb35d0294e
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-09 16:39:07 -0300

    :sparkles: validate target audience

--- test/CoursePlatform.DomainTest/Courses/CourseStoreTest.cs
+++ test/CoursePlatform.DomainTest/Courses/CourseStoreTest.cs
@@ -1,33 +1,55 @@
+using Bogus;
 using Bogus.DataSets;
 using CoursePlataform.Domain.Courses;
+using FluentAssertions;
 using Moq;
 
 namespace CoursePlataform.DomainTest.Courses;
 
 public class CourseStoreTest
 {
+    private readonly CourseDto _courseDto;
+    private readonly CourseStorage _courseStorage;
+    private readonly Mock<ICourseRepository> _courseRepositoryMock;
+
+    public CourseStoreTest()
+    {
+        var fake = new Faker();
+        _courseDto = new CourseDto
+        {
+            Name = fake.Random.Words(),
+            Description = fake.Lorem.Paragraph(),
+            Workload = fake.Random.Double(1, 50),
+            TargetAudience = "Employee",
+            Price = fake.Random.Double(1, 12000),
+        };
+        _courseRepositoryMock = new Mock<ICourseRepository>();
+        _courseStorage = new CourseStorage(_courseRepositoryMock.Object);
+    }
+
     [Fact]
     public void MustAddCourse()
     {
-        var courseDto = new CourseDto
-        {
-            Name = "Clean Architecture",
-            Description = "How to create a Web Api using Clean Architecture",
-            Workload = 24,
-            TargetAudience = TargetAudience.Employee,
-            Price = 1299,
-        };
-
-        var courseRepositoryMock = new Mock<ICourseRepository>();
+        _courseStorage.Add(_courseDto);
         
-        var courseStorage = new CourseStorage(courseRepositoryMock.Object);
+        _courseRepositoryMock.Verify(r => r.Add(
+            It.Is<Course>(
+                c => c.Name == _courseDto.Name &&
+                     c.Description == _courseDto.Description
+                )
+            ), Times.AtLeast(1));
+    }
 
-        courseStorage.Add(courseDto);
-        
-        courseRepositoryMock.Verify(r => r.Add(It.Is<Course>(c => c.Name == courseDto.Name)));
+    [Fact]
+    public void MustInformAValidTargetAudience()
+    {
+        _courseDto.TargetAudience = "Doctor";
+
+        Action action = () => _courseStorage.Add(_courseDto);
+        action.Should().Throw<ArgumentException>().WithMessage("Target audience invalid.");
     }
 }
- 
+
 public class CourseStorage
 {
     private readonly ICourseRepository _courseRepository;
@@ -39,8 +61,13 @@ public class CourseStorage
 
     public void Add(CourseDto courseDto)
     {
-        var course = new Course(courseDto.Name, courseDto.Workload, courseDto.TargetAudience, courseDto.Price,
+        bool tryParse = Enum.TryParse<TargetAudience>(courseDto.TargetAudience, out var targetAudience);
+
+        if (!tryParse) throw new ArgumentException("Target audience invalid.");
+
+        var course = new Course(courseDto.Name, courseDto.Workload, targetAudience, courseDto.Price,
             courseDto.Description);
+        
         _courseRepository.Add(course);
     }
 } 
@@ -55,6 +82,6 @@ public class CourseDto
     public string? Name { get; set; }
     public string? Description { get; set; }
     public double Workload { get; set; }
-    public TargetAudience TargetAudience { get; set; }
+    public string? TargetAudience { get; set; }
     public double Price { get; set; }
 }
