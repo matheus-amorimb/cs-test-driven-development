fe3caf027eb6c8c356602fc4b1ee5bcafcc5a9f7
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-10 16:26:55 -0300

    :sparkles: adjust architecture and folders

---
+++ TDDCoursePlatformApp/CoursePlatform.Ioc/ServiceExtension.cs
@@ -0,0 +1,26 @@
+using CoursePlataform.DomainTest.Courses;
+using CoursePlatform.Data.Context;
+using CoursePlatform.Data.Repositories.Implementations;
+using CoursePlatform.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CoursePlatform.Ioc;
+
+public static class ServiceExtension
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(builder =>
+        {
+            var postgresConnection = configuration.GetConnectionString("DefaultConnection"); 
+            builder.UseNpgsql(postgresConnection);
+        });
+        services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+        services.AddScoped<ICourseRepository, CourseRepository>();
+        services.AddScoped<CourseStorage>();
+        
+        return services;
+    }  
+}
