11f4d96096e904c8e4dd7be82cd339fafe02ac0e
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-10 21:04:36 -0300

    :sparkles: add method to get all courses

--- TDDCoursePlatformApp/src/CoursePlatform.Web/Controllers/CourseController.cs
+++ TDDCoursePlatformApp/src/CoursePlatform.Web/Controllers/CourseController.cs
@@ -1,17 +1,21 @@
+using CoursePlataform.Domain.Courses;
 using CoursePlataform.DomainTest.Courses;
+using CoursePlatform.Data.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoursePlatform.Web.Controllers;
 
 [ApiController]
-[Route("/api/[controller]")]
+[Route("/api/course")]
 public class CourseController : ControllerBase
 {
     private readonly CourseStorage _courseStorage;
+    private readonly IRepository<Course> _repository;
 
-    public CourseController(CourseStorage courseStorage)
+    public CourseController(CourseStorage courseStorage, IRepository<Course> repository)
     {
         _courseStorage = courseStorage;
+        _repository = repository;
     }
 
     [HttpPost]
@@ -21,5 +25,23 @@ public class CourseController : ControllerBase
         _courseStorage.Add(courseDto);
         
         return Ok(courseDto);
+    }
+
+    [HttpGet]
+    [Route("all")]
+    public async Task<ActionResult<IEnumerable<CourseDto>>> GetAll()
+    {
+        var courses = await _repository.GetAll();
+
+        var coursesDto = courses.Select(c => new CourseDto()
+        {
+            Name = c.Name, 
+            Description = c.Description,    
+            Workload = c.Workload,
+            TargetAudience = nameof(c.TargetAudience), 
+            Price = c.Price
+        });
+
+        return Ok(coursesDto);
     }
 }
