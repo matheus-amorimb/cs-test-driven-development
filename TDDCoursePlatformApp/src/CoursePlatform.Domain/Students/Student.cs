d3f65923dc7f96f6c37344bd7cb2edde376e5d79
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-14 10:44:42 -0300

    :sparkles: create domain entity student

---
+++ TDDCoursePlatformApp/src/CoursePlatform.Domain/Students/Student.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using CoursePlataform.Domain.Base;
+using CoursePlataform.Domain.Courses;
+using CoursePlataform.Domain.Utilities;
+
+namespace CoursePlataform.Domain.Students;
+
+public class Student : Entity
+{
+    public string? Name { get; private set; }
+    public string Cpf { get; private set; }
+    public string Email { get; private set; }
+    public TargetAudience TargetAudience { get; private set; }
+
+    public Student(string name, string cpf, string email, TargetAudience targetAudience)
+    {
+        Name = CheckName(name);
+        Cpf = CheckCpf(cpf);
+        Email = CheckEmail(email);
+        TargetAudience = targetAudience;
+    }
+
+    private string? CheckEmail(string email)
+    {
+        if (String.IsNullOrEmpty(email)) throw new ArgumentException(Resource.InvalidEmail);
+
+        return email;
+    }
+
+    private string? CheckCpf(string cpf)
+    {
+        if (String.IsNullOrEmpty(cpf)) throw new ArgumentException(Resource.InvalidCpf);
+
+        return cpf;
+    }
+
+    private string? CheckName(string name)
+    {
+        if (String.IsNullOrEmpty(name)) throw new ArgumentException(Resource.InvalidName);
+
+        return name;
+    }
+
+    public void ChangeName(string newName)
+    {
+        Name = CheckName(newName);
+    }
+}
