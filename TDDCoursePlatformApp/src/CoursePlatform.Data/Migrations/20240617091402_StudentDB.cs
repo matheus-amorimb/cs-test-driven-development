9e75b144dac9a9e1dced513410d16ae77fd92e80
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-19 07:34:12 -0300

    :sparkles: add student migrations

---
+++ TDDCoursePlatformApp/src/CoursePlatform.Data/Migrations/20240617091402_StudentDB.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoursePlatform.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StudentDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "student");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_student",
+                table: "student",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_student",
+                table: "student");
+
+            migrationBuilder.RenameTable(
+                name: "student",
+                newName: "Students");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "Id");
+        }
+    }
+}
